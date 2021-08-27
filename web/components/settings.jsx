81cad68ed873d00a3eace60e4a5783a838987bb5
PetruPractice <petru.duhlicher@mail.ru>
2021-08-28 01:43:43 +0300

    (UI Features) settings page

--- web/components/settings.jsx
+++ web/components/settings.jsx
@@ -1,13 +1,11 @@
-module.exports = ({emit}) => { 
-    const settingsPopup = <div class="row card modal modal-fixed-footer s12" id="settings">
+module.exports = () => (
+    <div class="row card modal modal-fixed-footer s12" id="settings">
             <div class="modal-content">
                 <p class="black">Settings</p>
             </div>
-            <div class="modal-footer">                
+            <div class="modal-footer">    
+                <a href="#!" class="modal-close waves-effect waves-green btn-flat">Cancel</a>            
                 <a href="#!" class="modal-close waves-effect waves-green btn-flat">Ok</a>
             </div>   
     </div>
-
-    M.Modal.init(settingsPopup , {})
-    return settingsPopup 
-}
+)
